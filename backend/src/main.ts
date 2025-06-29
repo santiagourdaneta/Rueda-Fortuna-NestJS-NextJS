6f2161880cc29c0d5243ed5d1499efb951e285ee
Santiago Urdaneta <santurdaneta@gmail.com>
2025-06-29 13:24:49 -0500

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,11 +6,11 @@ async function bootstrap() {
 
   // Configuración de CORS para permitir que el frontend hable con el backend
    app.enableCors({
-     origin: 'https://rueda-fortuna-nest-js-next-qo4p6vaug-santiagourdanetas-projects.vercel.app/', // ¡Permite que tu frontend hable con este backend!
+     origin: 'https://rueda-fortuna-nest-js-next-kydqykwmy-santiagourdanetas-projects.vercel.app/', // ¡Permite que tu frontend hable con este backend!
      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Qué tipo de peticiones se permiten
      credentials: true, // Si usaras cookies o sesiones, esto sería importante
    });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
